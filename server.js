cc4eb3851f6930dfd0d11e1bdb6a0949df258e3c
jprodriguez60@gmail.com <jprodriguez60@gmail.com>
2018-11-09 16:53:13 -0400

    added heroku configs

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.get('/',(req, res)=>{
 });
 
 function startServer() {
-    app.listen(3000);
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+    port = 8000;
+    }
+    app.listen(port);
     console.log("Server is up and running.\nOpen http://localhost:3000 to start");
 }
 
