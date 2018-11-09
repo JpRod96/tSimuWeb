0573fea95a39618456abc0d6ebef47bf506db7dd
jprodriguez60@gmail.com <jprodriguez60@gmail.com>
2018-11-09 16:46:48 -0400

    server handling root address

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.use(express.static("public"));
 
 app.use('/modules', express.static(__dirname + '/node_modules/'));
 
+app.get('/',(req, res)=>{
+    res.send('index.html');
+});
+
 function startServer() {
     app.listen(3000);
     console.log("Server is up and running.\nOpen http://localhost:3000 to start");
