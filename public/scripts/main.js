7e2d8b5651e5a5cedfde636e09774fa26059cc0e
JpRod <jprodriguez60@gmail.com>
2018-11-20 19:15:22 -0400

    Recuperando todos los eventos, registrando y viendo informe

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,7 @@
 const API_URL="http://pruebatsimu.herokuapp.com";
+//const API_URL="http://localhost:4567";
 const EVENTS_URL="/events";
+const EVENT_URL="/event";
 
 let app = angular.module("myApp", ["ngRoute", "ngAnimate"]);
 app.config(function($routeProvider) {
@@ -12,7 +14,7 @@ app.config(function($routeProvider) {
         templateUrl : "/formulario-denuncia.html",
         controller : "registroCtrl"
     })
-    .when("/ver/:pos", {
+    .when("/ver/:id", {
         templateUrl : "/resumen-instancia.html",
         controller : "resumenCtrl"
     })
@@ -27,10 +29,10 @@ app.controller("mainCtrl", function($scope, $http){
         method: 'GET',
         url: API_URL+EVENTS_URL,
     }
-    fetchDraftOrdersToScope($http,req,$scope);
+    fetchEventsScope($http,req,$scope);
 });
 
-function fetchDraftOrdersToScope(httpService, req, scope){
+function fetchEventsScope(httpService, req, scope){
     httpService(req)
     .then((response)=>{
         scope.events=response.data;
@@ -39,3 +41,58 @@ function fetchDraftOrdersToScope(httpService, req, scope){
         console.error(error);    
     });
 }
+
+app.controller("resumenCtrl", function($scope, $http, $routeParams){
+    let id=$routeParams.id;
+    let req = {
+        method: 'GET',
+        url: API_URL+EVENT_URL+"/"+id,
+    }
+    fetchEventScope($http,req,$scope);
+});
+
+function fetchEventScope(httpService, req, scope){
+    httpService(req)
+    .then((response)=>{
+        scope.event=response.data;
+    })
+    .catch((error)=>{
+        console.error(error);    
+    });
+}
+
+app.controller("registroCtrl", function($scope, $http, $window){
+    $scope.event={
+        eventDescription:"",
+        locationDescription:"",
+        longitude:0,
+        latitude:0,
+        timeMsEventDate:"",
+        initialState:""
+    };
+    $scope.submit = function() {
+        $scope.event.timeMsEventDate=$scope.event.timeMsEventDate.getTime();
+        let req = {
+            method: 'POST',
+            url: API_URL+EVENT_URL,
+            headers: {
+                'Content-Type': undefined
+            },
+            data: JSON.stringify($scope.event)
+        }
+        postEvent($http, req, $window);
+    };
+});
+
+function postEvent(httpService, req, window){
+    console.log(req);
+    httpService(req)
+    .then((response)=>{
+        window.location.href = '/';
+    })
+    .catch((error)=>{
+        console.error(error);
+    });
+}
+
+
